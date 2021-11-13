5ae9538852104f7318fb81770ce4204e0b0e93d4
Luan Costa <luansilvacosta2010@hotmail.com>
2021-11-13 10:19:00 -0300

    imagem em ASCII

---
+++ usar_no_wtsp/cnvrt_img_em_ASCII.py
@@ -0,0 +1,2 @@
+import pywhatkit as kit
+kit.image_to_ascii_art(r"C:\Users\luana\OneDrive\Imagens\301-3017270_128-kb-png-honk-honk-pepe-gif-clipart.png",R"C:\Users\luana\OneDrive\Área de Trabalho\ESTUDOS\python\ASCII3.txt")
